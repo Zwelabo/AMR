637102b33e04a39f58801b4c751bb6c8b426ecdf
Stanford Kwenda <stankwenda@gmail.com>
2025-03-16 01:44:25 +0200

    Create function for SIR interpretations using Gilberts logic

--- functions/amr_analysis_functions_01.R
+++ functions/amr_analysis_functions_01.R
@@ -1,5 +1,71 @@
-##functions
+# Functions ---------------------------------------------------------------
 
+# Function 1: Convert breakpoints to SIR
+
+convert2sir_fun <- function(df){
+  
+  df_len <- nrow(df)
+  if(df_len == 0){
+    stop("df seems empty.")
+  }
+  
+  res_list <- list()
+  
+  id_vec <- df %>% pull(int_id)
+  
+  for (i in id_vec){
+    
+    res_list[[i]] <- df %>% 
+      mutate() %>%
+      dplyr::filter(int_id==i)
+    
+    id=res_list[[i]][["int_id"]]
+    bacteria=res_list[[i]][["bacteria"]]
+    drug_code=as.ab(res_list[[i]][["drug_code"]])
+    test=res_list[[i]][["test_type"]]
+    guideline=res_list[[i]][["guideline"]]
+    value= as.numeric(res_list[[i]][["vals"]])
+    
+    
+    
+    if(test == 'mic'){
+      if(is.na(value)){
+        value=res_list[[i]][["vals"]]
+        interpreted <- as.character(as.sir(value, mo=paste0(bacteria), guideline = paste0(guideline), ab=paste0(drug_code)))
+      }else{
+        interpreted <- as.character(as.sir(as.mic(value), mo=paste0(bacteria), guideline = paste0(guideline), ab=paste0(drug_code)))
+      }
+    }else{
+      if(is.na(value)){
+        value=res_list[[i]][["vals"]]
+        interpreted <- as.sir(value, mo=paste0(bacteria), guideline = paste0(guideline), ab=paste0(drug_code))
+      }else{
+        interpreted <- as.sir(as.disk(value), mo=paste0(bacteria), guideline = paste0(guideline), ab=paste0(drug_code))
+      }
+    }
+    
+    
+    intrinsic_status <- as.character(mo_is_intrinsic_resistant( paste0(bacteria),ab=paste0(drug_code)))
+    
+    #update the df
+    #res_list[[i]][["drug_code"]]=paste0(drug_code) #keep unconformed drug for join back to demographics
+    res_list[[i]][["interpreted_res"]]=paste0(interpreted)
+    res_list[[i]][["intrinsic_res_status"]]=paste0(intrinsic_status)
+    
+  }
+  
+  
+  x<-dplyr::bind_rows(res_list)
+  
+  return(x)
+  
+}
+
+
+
+
+
+# Function 2: 
 amr_grp1_analysis= function(org_name, abs_ref, cntry, par, par_var_name){
 
   sel_abs <- subset(abs_ref,  abs_ref %in% names(an_df))
