8fff2bac592c446a32bda3b6f6c9f22e3f7e9dc5
Stanford Kwenda <stankwenda@gmail.com>
2025-03-16 13:02:47 +0200

    Add script to install requisite packages

---
+++ scripts/install_packages_packman.R
@@ -0,0 +1,46 @@
+# First install pacman
+
+if (!require("pacman", quietly = TRUE)){
+  install.packages("pacman")
+  library(pacman)
+}
+
+
+# Install the rest of the packages using pacman
+
+pacman::p_load(
+  AMR,
+  tidyverse,          
+  readxl,
+  remotes,       
+  ggplot2,
+  colorRamp2,
+  ggalt,
+  lubridate,
+  RColorBrewer,
+  openxlsx,
+  naniar,
+  glue,
+  readr,
+  readxl,
+  ggrepel,
+  ggtext,
+  BiocManager
+)
+
+
+# if (!require("ComplexHeatmap", quietly = TRUE)){
+#   BiocManager::install("ComplexHeatmap")
+#   library(ComplexHeatmap)
+# }
+# 
+# 
+# if (!require("tidyHeatmap", quietly = TRUE)){
+#   install.packages("tidyHeatmap")
+#   library(tidyHeatmap)
+# }
+# 
+# if (!require("ggtree", quietly = TRUE)){
+#   BiocManager::install("ggtree")
+#   library(ggtree)
+# }
