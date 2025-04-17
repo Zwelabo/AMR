7af53cfa51a31fe816f891ecdb7d2983d5c03c03
Gilbert Osena <gilbert.odindo.osena@gu.se>
2025-04-17 14:55:58 +0200

    Analysis and plotting functions updated

--- functions/amr_analysis_functions_01.R
+++ functions/amr_analysis_functions_01.R
@@ -62,34 +62,307 @@ convert2sir_fun <- function(df){
 }
 
 
+###Analysis functions
 
-
-# Function 2: Takes the pre-processed and interpreted data and analyzes the queried bug-drug combinations
-
-amr_grp1_analysis= function(org_name, abs_ref, cntry, par, par_var_name){
-
-  sel_abs <- subset(abs_ref,  abs_ref %in% names(an_df))
+amr_individual_pathogens <-  function(org_name, abs_ref, cntry, par, par_var_name){
+  sel_abs <- subset(abs_ref,  abs_ref %in% an_df_long$ab)
 
   #subgroups
-    hold_df <- an_df %>%
+  environment(indiv_ab_resistance) <- environment()
+  indiv_ab_resistance(an_df_long)
+
+  #calculating overall resistance
+  environment(overall_ab_resistance) <- environment()
+  overall_ab_resistance(an_df_long)
+
+  #class resistance
+  environment(antibiotic_classes_res_indiv) <- environment()
+  antibiotic_classes_res_indiv(an_df_long)
+}
+
+
+#MRSA
+mrsa_analysis <-  function(org_name, abs_ref, cntry, par, par_var_name){
+  mrsa_abs <- c('FOX', 'MET', 'OXA')
+  sel_abs_mrsa <- subset(mrsa_abs,  mrsa_abs %in% names(an_df))
+  sel_abs <- subset(abs_ref,  abs_ref %in% names(an_df))
+
+  #MRSA
+  #subgroups
+  environment(indiv_ab_resistance_sau) <- environment()
+  indiv_ab_resistance_sau(an_df_long)
+
+  #calculating overall resistance
+  #MRSA
+  environment(overall_ab_resistance_sau) <- environment()
+  overall_ab_resistance_sau(an_df_long)
+
+  #class resistance
+  environment(antibiotic_classes_res_indiv) <- environment()
+  antibiotic_classes_res_indiv(an_df_long)
+}
+
+##
+amr_pathogen_groups <-  function(org_name, abs_ref, cntry, par, par_var_name){
+  sel_abs <- subset(abs_ref,  abs_ref %in% an_df_long$ab)
+
+  #subgroups
+  environment(indiv_ab_resistance_genus) <- environment()
+  indiv_ab_resistance_genus(an_df_long)
+
+  #calculating overall resistance
+  environment(overall_ab_resistance_genus) <- environment()
+  overall_ab_resistance_genus(an_df_long)
+
+  #class resistance
+  environment(antibiotic_classes_res_grp) <- environment()
+  antibiotic_classes_res_grp(an_df_long)
+}
+#------------------------------------------------------------------------------------------------
+
+##Resistance calculation functions
+indiv_ab_resistance <- function(df){
+  hold_df <- df %>%
     filter(mo_organism==org_name) %>%
-    group_by(across(all_of(c('mo_organism', par_var_name)))) %>%
-    summarise(across(c(sel_abs),  #create a vector for each pathogen of interest
-                     list(n=n_sir,
-                          r=count_R,
-                          total_R = resistance#,
-                          #conf_int = function(x) sir_confidence_interval(x, collapse = "-")
-                     )))
+    group_by(mo_organism, get(par_var_name), ab) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_name(ab),
+           var_name=`get(par_var_name)`)
 
   write.csv(hold_df, paste0('outputs/',cntry,'_',org_name,'_',par,'.csv'))
 
-  hold_df_plot <- hold_df%>%
-    pivot_longer(names_to = 'variable', values_to = 'values',
-                 cols = names(hold_df)[3:length(hold_df)]) %>%                  #from col 3 onwards
-    mutate(ab=ab_name(ifelse(grepl('total', variable), str_split_i(variable, '_', 1), '')))
+  #plots
+  ggsave(paste0('plots/',cntry,'_',org_name,'_overall_abs','.png'), indiv_ab_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+}
 
-  plt_hold <- ggplot(hold_df_plot %>% drop_na(ab), aes(x=.data[[paste0(par_var_name)]], y=values*100, group=ab, fill=ab))+
+
+overall_ab_resistance <- function(df){
+  hold_df <- df %>%
+    filter(mo_organism==org_name) %>%
+    filter(ab %in% sel_abs) %>%
+    group_by( ab) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_name(ab))
+
+  write.csv(hold_df, paste0('Results/',cntry,'_',org_name,'_overall_abs.csv'))
+
+  ggsave(paste0('plots/',cntry,'_',org_name,'_overall_abs','.png'), overall_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+}
+
+
+indiv_ab_resistance_sau <- function(df){
+  hold_df_mrsa <- df %>%
+    filter(mo_organism==org_name) %>%
+    filter(ab %in% mrsa_abs) %>%
+    group_by(mo_organism, get(par_var_name)) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name='MRSA',
+           var_name=`get(par_var_name)`)
+
+
+  write.csv(hold_df_mrsa, paste0('outputs/',cntry,'_',org_name,'_mrsa',par,'.csv'))
+
+
+  #other combos
+  hold_df_a <- df %>%
+    filter(mo_organism==org_name) %>%
+    filter(ab %in% sel_abs) %>%
+    group_by(mo_organism, get(par_var_name), ab) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_name(ab),
+           var_name=`get(par_var_name)`)
+
+  write.csv(hold_df_a, paste0('outputs/',cntry,'_',org_name,'_',par,'.csv'))
+
+  hold_df <- bind_rows(hold_df_a, hold_df_mrsa)
+
+  ggsave(paste0('plots/',cntry,'_',org_name,'_overall_abs','.png'), indiv_ab_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+}
+
+overall_ab_resistance_sau <- function(df){
+  hold_df_mrsa <- df %>%
+    filter(mo_organism==org_name) %>%
+    filter(ab %in% mrsa_abs) %>%
+    group_by( mo_organism) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name='MRSA')
+
+
+  write.csv(hold_df_mrsa, paste0('Results/',cntry,'_',org_name,'mrsa_overall_abs.csv'))
+
+  #other combos
+  hold_df_a <- df %>%
+    filter(mo_organism==org_name) %>%
+    group_by( ab) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_name(ab))
+
+
+  write.csv(hold_df_a, paste0('Results/',cntry,'_',org_name,'_overall_abs.csv'))
+
+  hold_df <- bind_rows(hold_df_a, hold_df_mrsa)
+
+  #plots
+  ggsave(paste0('plots/',cntry,'_',org_name,'_overall_abs','.png'), overall_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+}
+
+  #pathogen groups
+indiv_ab_resistance_genus <- function(df){
+    hold_df <- df %>%
+      mutate(mo_organism=genus) %>%
+      filter(ab %in% sel_abs) %>%
+      filter(mo_organism==org_name) %>%
+      group_by(mo_organism, get(par_var_name), ab) %>%
+      summarise(n=n(),
+                r=sum(R),
+                total_R = r/n  ) %>%
+      mutate(ab_name=ab_name(ab),
+             var_name=`get(par_var_name)`)
+
+    write.csv(hold_df, paste0('outputs/',cntry,'_',org_name,'_',par,'.csv'))
+
+    #plots
+    ggsave(paste0('plots/',cntry,'_',org_name,'_overall_abs','.png'), indiv_ab_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+  }
+
+
+overall_ab_resistance_genus <- function(df){
+    hold_df <- df %>%
+      mutate(mo_organism=genus) %>%
+      filter(mo_organism==org_name) %>%
+      filter(ab %in% sel_abs) %>%
+      group_by( ab) %>%
+      summarise(n=n(),
+                r=sum(R),
+                total_R = r/n  ) %>%
+      mutate(ab_name=ab_name(ab))
+
+    write.csv(hold_df, paste0('Results/',cntry,'_',org_name,'_overall_abs.csv'))
+
+  #plots
+  ggsave(paste0('plots/',cntry,'_',org_name,'_overall_abs','.png'),overall_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+}
+
+
+
+#Antibiotic class resistance
+
+#Individual pathogens
+antibiotic_classes_res_indiv <- function(df) {
+
+  abx_classes <- unique(ab_group(abs_ref)) #returns the antibiotic classes
+
+  hold_df <- df %>% filter(ab_class %in% abx_classes) %>%
+    filter(mo_organism==org_name) %>%
+    arrange(desc(R)) %>%
+    distinct(uid,mo_organism,ab_class, .keep_all = TRUE) %>% #remove duplicates for the Ab classes, depending on how vast the dataset is, could be updated to country, lab, etc
+    group_by(mo_organism, get(par_var_name), ab_class) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_class,
+           var_name=`get(par_var_name)`)
+
+  write.csv(hold_df, paste0('outputs/',cntry,'_',org_name,'_',par,cntry,'ab_classes.csv'))
+
+  #plot
+  ggsave(paste0('plots/',cntry,'_',org_name,'_',par,cntry,'ab_classes.png'), indiv_ab_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+
+
+  #calculating overall resistance
+  hold_df <- df %>%
+    filter(mo_organism==org_name) %>%
+    filter(ab_class %in% abx_classes) %>%
+    arrange(desc(R)) %>%
+    distinct(uid,mo_organism,ab_class, .keep_all = TRUE) %>% #remove duplicates for the Ab classes, depending on how vast the dataset is, could be updated to country, lab, etc
+    group_by(mo_organism, ab_class) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_class)
+
+  write.csv(hold_df, paste0('Results/',cntry,'_',org_name,'_overall_ab_classes.csv'))
+
+  #plot
+  ggsave(paste0('plots/',cntry,'_',org_name,cntry,'_overall_ab_classes','.png'), overall_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+}
+
+
+#Grouped pathogens
+antibiotic_classes_res_grp <- function(df) {
+
+  abx_classes <- unique(ab_group(abs_ref)) #returns the antibiotic classes
+
+  hold_df <- df %>% filter(ab_class %in% abx_classes) %>%
+    mutate(mo_organism=genus) %>%
+    filter(mo_organism==org_name) %>%
+    arrange(desc(R)) %>%
+    distinct(uid,mo_organism,ab_class, .keep_all = TRUE) %>% #remove duplicates for the Ab classes, depending on how vast the dataset is, could be updated to country, lab, etc
+    group_by(mo_organism, get(par_var_name), ab_class) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_class,
+           var_name=`get(par_var_name)`)
+
+  write.csv(hold_df, paste0('outputs/',cntry,'_',org_name,'_',par,cntry,'ab_classes.csv'))
+
+  #plot
+  ggsave(paste0('plots/',cntry,'_',org_name,'_',par,cntry,'ab_classes.png'), indiv_ab_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+
+
+  #calculating overall resistance
+  hold_df <- df %>%
+    mutate(mo_organism=genus) %>%
+    filter(mo_organism==org_name) %>%
+    filter(ab_class %in% abx_classes) %>%
+    arrange(desc(R)) %>%
+    distinct(uid,mo_organism,ab_class, .keep_all = TRUE) %>% #remove duplicates for the Ab classes, depending on how vast the dataset is, could be updated to country, lab, etc
+    group_by(mo_organism, ab_class) %>%
+    summarise(n=n(),
+              r=sum(R),
+              total_R = r/n  ) %>%
+    mutate(ab_name=ab_class)
+
+  write.csv(hold_df, paste0('Results/',cntry,'_',org_name,'_overall_ab_classes.csv'))
+
+  #plot
+  ggsave(paste0('plots/',cntry,'_',org_name,cntry,'_overall_ab_classes','.png'), overall_resistance_plot(hold_df), width=8, height=8, units="in", dpi=300)
+}
+
+#-------------------------------------------------------------------------------------------
+
+#plotting functions
+overall_resistance_plot <- function(df) {
+  plt_hold <- ggplot(df  %>% filter(n>29), aes(x=reorder(ab_name,total_R), y=total_R*100, group=ab_name))+
+    geom_col(position = 'dodge', fill='dodgerblue')+
+    labs(x='Antibiotic', y='Percent resistance')+
+    theme_bw()+
+    theme(axis.text.x = element_text(size=11,angle=90,hjust=1,vjust=0),axis.text.y = element_text(size=10),axis.title = element_text(size=12),
+          axis.title.x = element_text(size=11),
+          plot.title = element_text(hjust = 0.5, face='bold'),legend.key.size = unit(0.7, "cm"),legend.spacing.x = unit(0.5,"cm"),
+          strip.text = element_text(size=11),legend.text=element_text(size=8),legend.title = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA, size=0.5),legend.position='right')
+ plt_hold
+}
+
+
+indiv_ab_resistance_plot <- function(df) {
+  plt_hold <- ggplot(df %>% filter(n>29), aes(x=var_name, y=total_R*100, group=ab_name, fill=ab_name))+
     geom_col( position = 'dodge')+
+    #geom_text(aes( y=total_R*100,label=round(total_R*100,1)), position=position_dodge(width = 0.9), vjust=-.5)+
     labs(x=par, y='Percent resistance')+
     theme_bw()+
     theme(axis.text.x = element_text(size=11,angle=0,hjust=0.5,vjust=0.5),axis.text.y = element_text(size=10),axis.title = element_text(size=12),
@@ -98,41 +371,8 @@ amr_grp1_analysis= function(org_name, abs_ref, cntry, par, par_var_name){
           plot.title = element_text(hjust = 0.5, face='bold'),legend.key.size = unit(0.7, "cm"),legend.spacing.x = unit(0.5,"cm"),
           strip.text = element_text(size=11),legend.text=element_text(size=8),legend.title = element_blank(),
           panel.border = element_rect(colour = "black", fill=NA, size=0.5),legend.position='bottom')
-
-
-  ggsave(paste0('plots/',cntry,'_',org_name,'_',par,'.png'), plt_hold, width=8, height=8, units="in", dpi=300)
-
-
-  #calculating overall resistance
-  hold_df <- an_df %>%
-    filter(mo_organism==org_name) %>%
-    summarise(across(c(sel_abs),  #create a vector for each pathogen of interest
-                     list(n=n_sir,
-                          r=count_R,
-                          total_R = resistance#,
-                          #conf_int = function(x) sir_confidence_interval(x, collapse = "-")
-                     )))
-
-  write.csv(hold_df, paste0('Results/',cntry,'_',org_name,'_overall.csv'))
-
-  hold_df_plot <- as.tibble(t(hold_df))%>%
-    mutate(variable=names(hold_df),
-           values=V1,
-           ab=ab_name(ifelse(grepl('total', variable), str_split_i(variable, '_', 1), '')))
-
-  plt_hold <- ggplot(hold_df_plot %>% drop_na(ab), aes(x=ab, y=values*100, group=ab))+
-    geom_col( position = 'dodge', fill='dodgerblue')+
-    labs(x='Antibiotic', y='Percent resistance')+
-    theme_bw()+
-    theme(axis.text.x = element_text(size=11,angle=90,hjust=1,vjust=0),axis.text.y = element_text(size=10),axis.title = element_text(size=12),
-          axis.title.x = element_text(size=11),
-          #panel.grid = element_blank(),
-          plot.title = element_text(hjust = 0.5, face='bold'),legend.key.size = unit(0.7, "cm"),legend.spacing.x = unit(0.5,"cm"),
-          strip.text = element_text(size=11),legend.text=element_text(size=8),legend.title = element_blank(),
-          panel.border = element_rect(colour = "black", fill=NA, size=0.5),legend.position='right')
-
-
-  ggsave(paste0('plots/',cntry,'_',org_name,'_overall','.png'), plt_hold, width=8, height=8, units="in", dpi=300)
-
-
+  plt_hold
 }
+#------------------------------------------------------------------------------------------------------
+
+
