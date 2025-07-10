912be81a8d9e858671937bdc31341f9dc343fa41
Allan Abala <abala.allan@gmail.com>
2025-07-10 08:08:22 +0300

    Update AMU_analysis.R

--- scripts/AMU_analysis.R
+++ scripts/AMU_analysis.R
@@ -3,6 +3,7 @@ library(readxl)
 library(dplyr)
 library(tidyverse)
 library(writexl)
+library(ggplot2)
 
 # Load the AMU dataset
 amu_path <- "test-data/AMU/Merged_Point Prevalence Survey_testing.xlsx"
@@ -78,22 +79,83 @@ amu_dataset <- amu_dataset %>%
 ############## Creating tables based on formulas for amu
 ##########################################################
 
-# Demographics - % of surveyed patients by AgeGroup
+############### Demographics - % of surveyed patients by AgeGroup
 demographics_age <- amu_dataset %>%
   distinct(facility, Patient_UniqueID, AgeGroup) %>%
   count(facility, AgeGroup) %>%
   group_by(facility) %>%
   mutate(Percent = round(n / sum(n) * 100, 2))
 
-# Save to Excel
-write_xlsx(demographics_age, path = "plots_AMU/demographics_by_agegroup.xlsx")
+write_xlsx(demographics_age, path = "plots_AMU/demographics_by_agegroup.xlsx") # Save to Excel
 
-# Demographics - % of surveyed patients by Gender
+#################### Demographics - % of surveyed patients by Gender
 demographics_gender <- amu_dataset %>%
   distinct(facility, Patient_UniqueID, Gender) %>%
   count(facility, Gender) %>%
   group_by(facility) %>%
   mutate(Percent = round(n / sum(n) * 100, 2))
 
-# Save to Excel
-write_xlsx(demographics_gender, path = "plots_AMU/demographics_by_gender.xlsx")
+write_xlsx(demographics_gender, path = "plots_AMU/demographics_by_gender.xlsx") # Save to Excel
+
+################## AMU Prevalence - overall, by age group, by gender
+amu_flags <- amu_dataset %>%
+  mutate(On_AM = !is.na(Antimicrobial_Name)) %>%
+  distinct(facility, Patient_UniqueID, Gender, AgeGroup, On_AM)
+
+## overall prevalence
+prevalence_overall <- amu_flags %>%
+  group_by(facility) %>%
+  summarise(AMU_Prev = round(sum(On_AM) / n() * 100, 2))
+
+write_xlsx(prevalence_overall, path = "plots_AMU/prevalence_overall_by_facility.xlsx") # Save to Excel
+
+# Bar chart of AMU prevalence by site
+amu_plot <- ggplot(prevalence_overall, aes(x = facility, y = AMU_Prev, fill = facility)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Antimicrobial Use Prevalence by Site",
+    x = "Site",
+    y = "AMU Prevalence (%)"
+  ) +
+  theme_classic() +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))  # Vertical alignment)
+
+# Save the plot to the plots_AMU folder
+ggsave(filename = "plots_AMU/AMU_prevalence_by_site.png", plot = amu_plot, width = 10, height = 6, dpi = 300)
+
+## prevalence by age
+prevalence_by_age <- amu_flags %>%
+  group_by(facility, AgeGroup) %>%
+  summarise(AMU_Prev = round(sum(On_AM) / n() * 100, 2))
+
+write_xlsx(prevalence_by_age, path = "plots_AMU/prevalence__by_age.xlsx") # Save to Excel
+
+## prevalence by gender
+prevalence_by_gender <- amu_flags %>%
+  group_by(facility, Gender) %>%
+  summarise(AMU_Prev = round(sum(On_AM) / n() * 100, 2))
+
+write_xlsx(prevalence_by_gender, path = "plots_AMU/prevalence_by_gender.xlsx") # Save to Excel
+
+##################### AMU Prevalence by Ward
+prevalence_by_ward <- amu_dataset %>%
+  distinct(facility, Patient_UniqueID, WardName, Antimicrobial_Name) %>%
+  mutate(On_AM = !is.na(Antimicrobial_Name)) %>%
+  group_by(facility, WardName) %>%
+  summarise(AMU_Prev = round(sum(On_AM) / n() * 100, 2))
+
+# Bar chart of AMU prevalence by Ward
+amu_plot <- ggplot(prevalence_by_ward, aes(x = WardName, y = AMU_Prev, fill = facility)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "Antimicrobial Use Prevalence by Hospital Ward",
+    x = "Ward",
+    y = "AMU Prevalence (%)"
+  ) +
+  theme_classic() +
+  theme(legend.position = 'none',
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))  # Vertical alignment
+
+# Save the plot to the plots_AMU folder
+ggsave(filename = "plots_AMU/AMU_prevalence_by_Ward.png", plot = amu_plot, width = 10, height = 6, dpi = 300)
